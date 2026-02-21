7ed195d40894258cf844d35570b20591bfe6cd6c
mrabomb <makeanewname@gmail.com>
2026-02-20 20:47:21 -0600

    turns the webapp into a page

--- web.py
+++ web.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 import eldritchify as e
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world(inStr: str):
-    return "<p>" + e.curseText(inStr) + "</p>"
+
+@app.route('/eldritchify', methods=["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+        # getting input with name = "user_input" in HTML form
+        user_text = request.form.get("user_input")
+
+        processed_text = e.curseText(user_text)
+        return render_template("form.html", result=processed_text)
+
+    # For GET request, render the form page
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
