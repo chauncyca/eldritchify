3b4fea7bfcebdb94e77fe7e408d3d8c14fe60186
mrabomb <makeanewname@gmail.com>
2026-02-20 21:28:52 -0600

    wraps in a try catch

--- web.py
+++ web.py
@@ -7,15 +7,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def gfg():
-    if request.method == "POST":
-        # getting input with name = "user_input" in HTML form
-        user_text = request.form.get("user_input")
+    try:
+        if request.method == "POST":
+            # getting input with name = "user_input" in HTML form
+            user_text = request.form.get("user_input")
 
-        processed_text = e.curseText(user_text)
-        return render_template("form.html", result=processed_text)
+            processed_text = e.curseText(user_text)
+            return render_template("form.html", result=processed_text)
 
-    # For GET request, render the form page
-    return render_template("form.html")
+        # For GET request, render the form page
+        return render_template("form.html")
+    except:
+        return ""
 
 if __name__ == "__main__":
     app.run()
